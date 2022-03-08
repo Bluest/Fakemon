b71d0a313618dde8590a17611002311dfef85237
Oscar <oscarlh24@hotmail.com>
2022-03-08 02:49:03 +0000

    Add Type enum and Move class

---
+++ Assets/Move.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Move
+{
+    string name;
+    Type type;
+    Action effect;
+}
--- Assets/Pokemon.cs
+++ Assets/Pokemon.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +10,13 @@ public class Pokemon : MonoBehaviour
     int currentHP = 20;
     int attack = 4;
     int defence = 0;
+    List<Move> moves = new List<Move>();
+    
+    void Start()
+    {
+        Move tackle = new Move();
+        moves.Add(tackle);
+    }
 
     public void TakeDamage(int damage)
     {
