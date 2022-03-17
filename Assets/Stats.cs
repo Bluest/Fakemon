4e3a18601adfe40b9d652e3a57c5ae3543cd9dcd
Oscar <oscarlh24@hotmail.com>
2022-03-17 23:45:53 +0000

    Add stat stages

--- Assets/Stats.cs
+++ Assets/Stats.cs
@@ -1,18 +1,28 @@
 public struct Stats
 {
     public int baseAttack;
-    public int baseDefense;
-    public int baseSpecialAttack;
-    public int baseSpecialDefense;
-    public int baseSpeed;
+    public int attackStage;
     public int currentAttack;
+    public int baseDefense;
+    public int defenseStage;
     public int currentDefense;
+    public int baseSpecialAttack;
+    public int specialAttackStage;
     public int currentSpecialAttack;
+    public int baseSpecialDefense;
+    public int specialDefenseStage;
     public int currentSpecialDefense;
+    public int baseSpeed;
+    public int speedStage;
     public int currentSpeed;
 
     public void ResetAll()
     {
+        attackStage = 0;
+        defenseStage = 0;
+        specialAttackStage = 0;
+        specialDefenseStage = 0;
+        speedStage = 0;
         currentAttack = baseAttack;
         currentDefense = baseDefense;
         currentSpecialAttack = baseSpecialAttack;
